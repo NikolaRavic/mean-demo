cfd9b3b18456d3c9eddeb7827410b5f626e6dabe
Nikola Ravic <nikola.ravic@gmail.com>
2017-02-08 16:04:20 +0100

    -added feature to move images to archive folder on DELETE request

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
 var public_root = '../app',
-    image_root = '/images';
+    image_root = '../app/images';
 
 app.use(express.static(public_root));
 
@@ -12,13 +12,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/get-all-images', function (req, res) {
-    var allImages = getImages(public_root + image_root);
+    var allImages = getImages(image_root);
     res.send(JSON.stringify(allImages));
 });
 
 app.post('/delete-image', function (req, res) {
-    console.log(req.body.params.image);
-    res.send('Deleted');
+
+    var image = req.body.params.image;
+
+    fs.rename(public_root + '/' + image, image_root + '/archive/' + path.basename(image), function (err) {
+        if(err){
+            console.log(err);
+            res.send(err);
+        }else {
+            res.send('Deleted');
+            console.log("Image " + image + " moved to archive folder!");
+        }
+    });
 });
 
 app.listen(1337, function () {
